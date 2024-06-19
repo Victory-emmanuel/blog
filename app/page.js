21b9540025a25051db290bc6b5925ca3643a744e
Victory-emmanuel <marketinglot-blog@gmail.com>
2024-06-19 10:18:25 +0100

    commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <p className=""> Next js </p>{" "}
+      </div>{" "}
+    </>
+  );
+}
